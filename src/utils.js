7f946dad0ed996fcb96b3b77cc8d5e13ac25a7e7
Alexander-Morlock <onlinemania@mail.ru>
2020-12-14 00:42:35 +0100

    module5-task1

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import Abstract from "./view/abstract.js";
+
 export const RenderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
@@ -54,4 +56,22 @@ export const getDuration = (timeStart, timeEnd) => {
   duration += minutes + `M`;
 
   return duration;
+};
+
+export const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error(`Can't replace unexisting elements`);
+  }
+
+  parent.replaceChild(newChild, oldChild);
 };
