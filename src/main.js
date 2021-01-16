bf02c4c60d3c29bf39d5fd4c38111cec8c7d17c9
Alexander-Morlock <onlinemania@mail.ru>
2021-01-16 10:43:18 +0100

    module7-task2-07

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import TripPresenter from './presenter/trip.js';
 import PointsModel from './model/points.js';
 import ControlsPresenter from './presenter/controls.js';
 import ControlsModel from './model/controls.js';
-import {getGeneratedPoint} from './mock/mockdata.js';
-import {render, RenderPosition} from './utils/render.js';
+import { getGeneratedPoint } from './mock/mockdata.js';
+import { render, RenderPosition } from './utils/render.js';
 
 const NUMBER_OF_LIST_ITEMS = 3;
 const pointsData = new Array(NUMBER_OF_LIST_ITEMS).fill().map(getGeneratedPoint);
@@ -17,24 +17,28 @@ const pointsModel = new PointsModel();
 pointsModel.setPoints(pointsData);
 
 const updateRouteInfo = (points) => {
-  if (points.length) {
-    if (headerContainer.children[0]) {
-      headerContainer.children[0].remove();
-    }
-    render(headerContainer, new RouteInfoView(points).getElement(), RenderPosition.AFTERBEGIN);
-    const renderControlsAfterThisElement = document.querySelector(`.trip-main__trip-info`);
-    const controlsPresenter = new ControlsPresenter(renderControlsAfterThisElement, controlsModel);
-    controlsPresenter.init();
-  } else {
-    if (pointsModel.getPoints().length) {
-      document.querySelector(`.trip-info__title`).textContent = `No results for «${controlsModel.getFilter()}» filter`.toUpperCase();
-      document.querySelector(`.trip-info__dates`).textContent = `No dates to display`;
-      document.querySelector(`.trip-info__cost-value`).textContent = `0`;
-    } else {
-      headerContainer.children[0].remove();
-      render(headerContainer, new EmptyListHeaderView().getElement(), RenderPosition.AFTERBEGIN);
-      tripEventsContainer.append(new NoPointsView().getElement());
-      tripEventsContainer.querySelector(`form`).remove();
+  switch (points.length > 0) {
+    case true:
+      if (headerContainer.children[0]) {
+        headerContainer.children[0].remove();
+      }
+      render(headerContainer, new RouteInfoView(points).getElement(), RenderPosition.AFTERBEGIN);
+      const renderControlsAfterThisElement = document.querySelector(`.trip-main__trip-info`);
+      const controlsPresenter = new ControlsPresenter(renderControlsAfterThisElement, controlsModel);
+      controlsPresenter.init();
+      break;
+
+    case false: {
+      if (pointsModel.getPoints().length) {
+        document.querySelector(`.trip-info__title`).textContent = `No results for «${controlsModel.getFilter()}» filter`.toUpperCase();
+        document.querySelector(`.trip-info__dates`).textContent = `No dates to display`;
+        document.querySelector(`.trip-info__cost-value`).textContent = `0`;
+      } else {
+        headerContainer.children[0].remove();
+        render(headerContainer, new EmptyListHeaderView().getElement(), RenderPosition.AFTERBEGIN);
+        tripEventsContainer.append(new NoPointsView().getElement());
+        tripEventsContainer.querySelector(`form`).remove();
+      }
     }
   }
 };
